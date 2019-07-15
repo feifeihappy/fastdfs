e2e7e707e04b04578b90f8154badec6b0bb32e8f
zpf <zhaopf@mti-sh.cn>
2019-07-15 11:59:25 +0800

    修改相应信息

---
+++ src/main/java/com/example/fastdfs/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.example.fastdfs.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @Description: 测试
+ * @Author: zhaopf@mti-sh.cn
+ * @Date: 2019/7/5 10:47
+ */
+@RestController
+@Slf4j
+public class TestController {
+
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String upload() {
+
+        return "请求成功";
+    }
+
+
+}
